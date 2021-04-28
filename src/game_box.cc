e12162580a6d8c75399e3752eea89c9382b0e10d
nvacharya2 <42473577+nvacharya2@users.noreply.github.com>
2021-04-27 19:24:52 -0500

    Bricks displayed successfully.

--- src/game_box.cc
+++ src/game_box.cc
@@ -14,19 +14,50 @@ namespace brickbreaker {
                             vec2 (paddle_right_, paddle_bottom_));
 
         int max_strength_level = 4; //# of brick levels
-        int bricks_per_row = 10;
+        int bricks_per_row = 7; //10
 
         //takes care of x/row spacing
         int brick_length = game_box_length_ / bricks_per_row;
-        int end_x_prev_brick;
+        //int end_x_prev_brick;
 
         //takes care of y/column spacing
         int brick_height = brick_space_y / max_strength_level;
 
+        int brick_left;
+        int brick_right;
+        int brick_upper;
+        int brick_lower;
+
         for (int i = 1; i <= max_strength_level; i++) {
             for (int j = 0; j < bricks_per_row; j++) {
-                //Brick();
-                bricks_.push_back(Brick(i)); //i = strength (1 thru 4)
+
+                if (j == 0) {
+                    brick_left = left_wall_ + (j * brick_length);
+                    brick_right = brick_left + brick_length;
+                    brick_upper = upper_wall_ + (i * brick_height); //(i - 1)
+                    brick_lower = brick_upper + brick_height;
+
+                } else {
+                    brick_left = left_wall_ + (j * brick_length); // + 1;
+                    brick_right = brick_left + brick_length;
+                    brick_upper = upper_wall_ + (i * brick_height); //(i - 1)
+                    brick_lower = brick_upper + brick_height; // + 1;
+
+
+                    /*
+                    int brick_left = left_wall_ + (j * brick_length) + 1;
+                    int brick_right = brick_length + brick_length;
+                    int brick_upper = upper_wall_ + (i * brick_height);
+                    int brick_lower = brick_upper + brick_height + 1;
+                     */
+
+                }
+
+                // FIX STRENGTH ASSIGNMENT, INCORRECT!
+                bricks_.push_back(Brick(brick_left, brick_right, brick_upper, brick_lower, i)); //incorrect i value
+
+                //Brick(brick_left, brick_right, brick_upper, brick_lower, i);
+                //bricks_.push_back(Brick(i)); //i = strength (1 thru 4)
             }
         }
 
@@ -57,21 +88,56 @@ namespace brickbreaker {
                                                   vec2(right_wall_, lower_wall_)));
             }
         }*/
+
         int color_switch = 0;
+        int color_spacer = 255 / bricks_.size();
+        int color_multiplier = 0;
+
         for(const Brick &brick : bricks_) {
+
+            /*
             if (color_switch % 2 == 0) {
-                ci::gl::color(ci::Color("yellow"));
-                /*
-                ci::gl::drawStrokedRect(ci::Rectf(vec2(left_wall_, upper_wall_),
-                                                  vec2(right_wall_, lower_wall_)));*/
+                //ci::gl::color(ci::Color("yellow"));
+
+                ci::gl::color(ci::Color8u(255 - (color_multiplier * color_spacer),
+                                          178, (color_multiplier * color_spacer)));
+
+                ci::gl::color(ci::Color8u(255 - (color_multiplier * color_spacer),
+                                          178,
+                                          (color_multiplier * color_spacer)));
+                //green: (color_multiplier * color_spacer)
+
 
             } else {
-                ci::gl::color(ci::Color("green"));
-                /*
-                ci::gl::drawStrokedRect(ci::Rectf(vec2(left_wall_, upper_wall_),
-                                                  vec2(right_wall_, lower_wall_)));*/
+                //ci::gl::color(ci::Color("green"));
+                //ci::gl::color(ci::Color8u(255, 178, 112));
+
+                ci::gl::color(ci::Color8u(255 - (color_multiplier * color_spacer),
+                                          178, (color_multiplier * color_spacer)));
+
+                ci::gl::color(ci::Color8u(255 - (color_multiplier * color_spacer),
+                                          178,
+                                          (color_multiplier * color_spacer) / 2));
+
+            }*/
+            if (color_switch % 2 == 0) {
+                ci::gl::color(ci::Color8u(220, // / 2, // - (color_multiplier * color_spacer),
+                                          0, (color_multiplier * color_spacer)));
+
+            } else {
+                ci::gl::color(ci::Color8u(220, // - (color_multiplier * color_spacer),
+                                          75, (color_multiplier * color_spacer)));
+
             }
-            ci::gl::drawStrokedRect(ci::Rectf(brick.GetLeftUpperBound(), brick.GetRightLowerBound()));
+
+            /*
+            ci::gl::color(ci::Color8u(255, // - (color_multiplier * color_spacer),
+                                      178, (color_multiplier * color_spacer)));*/
+
+            //ci::gl::drawStrokedRect(ci::Rectf(brick.GetLeftUpperBound(), brick.GetRightLowerBound()));
+            ci::gl::drawSolidRect(ci::Rectf(brick.GetLeftUpperBound(), brick.GetRightLowerBound()));
+
+            color_multiplier++;
             color_switch++;
         }
     }
